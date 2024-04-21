e58a260c533d2e8fa69b0ceafc74371b3b7c1a50
Yarik0310 <akela0206@gmail.com>
2024-04-21 17:51:16 +0300

    Site init

---
+++ src/layout/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+return <nav className="indigo darken-1">
+    <div className='container'>
+        <div class="nav-wrapper">
+        <a href="!#" 
+        className="brand-logo">React Movies</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="!#">Repository</a></li>
+        </ul>
+        </div>
+    </div>
+  </nav>
+}
+export { Header };
